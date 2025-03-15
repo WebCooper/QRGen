8addb9bc9d25269d3b88758795306cce301073cb
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-03-15 20:41:08 +0530

    test for webhook5

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World with Jenkins and Docker! ');
+  res.send('Hello, World with Jenkins and Docker! and more optimized');
 });
 
 app.listen(port, () => {
