11ae5bfc19987dc76ebc39d0f97ce0d336f81122
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-04-01 18:55:23 +0530

    add secrets

--- Frontend/utils/axiosInstance.ts
+++ Frontend/utils/axiosInstance.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/auth';
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: `${API_URL}/api/auth`,
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+    withCredentials: true // Required for CORS with credentials
+});
 
 export default axiosInstance;
